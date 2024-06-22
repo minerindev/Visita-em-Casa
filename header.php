c8f1291003912c211c9ec49b189c16538bcbe2bb
minerindev <jgespolador@gmail.com>
2024-06-22 01:16:48 -0300

    Header e Footer criados

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="home.css">
+    <title>Vacina em Casa!</title>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+
+<body>
+
+    <nav class="first_nav">
+        <span style="font-size:20px; margin:auto;">Fique atento ao Calendário de Vacinação</span>
+    </nav>
+    
+    <nav class="sec_nav">
+
+        <img src="imagens/logo-nav.png" alt="Nykaa">
+
+        <div>
+            <p></p><a href="https://www.gov.br/pt-br/orgaos-do-governo"><strong>Órgãos do Governo</strong></a>
+            <p></p><a href="https://www.gov.br/acessoainformacao/pt-br"><strong>Acesso à Informação</strong></a>
+            <p></p><a href="https://www4.planalto.gov.br/legislacao"><strong>Legislação</strong></a>
+            <p></p><a href="https://www.gov.br/governodigital/pt-br/acessibilidade-e-usuario/acessibilidade-digital"><strong>Acessibilidade</strong></a>
+            <p></p><a href="sobre_nos.html"><strong>Sobre Nós</strong></a>
+        </div>
+
+        <div>
+            <div class="d-grid gap-2 d-md-block">
+                <a href="../login/login.php" class="btn btn-orange" role="button">Login</a>
+                <a href="../cadastro/cadastro.php" class="btn btn-orange" role="button">Cadastrar-se</a>
+            </div>
+        </div>
+    </nav>
